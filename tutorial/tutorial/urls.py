4839c2b6493cf4edfbcf3101ebf5fcfc18c5d976
trong63 <bryan_tran@comcast.net>
2020-02-19 12:22:11 -0600

    @api_view and class base view'

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -20,10 +20,32 @@ Including another URLconf
 #     path('admin/', admin.site.urls),
 # ]
 
+####################################################################
+#BT - @api_view
+####################################################################
+# from django.urls import path
+# from snippets import views
+
+# from rest_framework.urlpatterns import format_suffix_patterns
+
+# urlpatterns = [
+#     path('snippets/', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+####################################################################
+#BT - Class base view
+####################################################################
+
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
