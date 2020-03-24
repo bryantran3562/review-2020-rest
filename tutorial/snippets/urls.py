28a1d1b69d7db94449f82b7365e7f5bcd9b23f73
bryantran3562 <bryantran3562@gmail.com>
2020-03-24 07:27:07 -0500

    restframework-ApiView

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,14 +1,29 @@
-####################################################################
-#BT - @api_view
-####################################################################
+# ####################################################################
+# #BT - @api_view
+# ####################################################################
+# from django.urls import path
+# from snippets import views
+
+# from rest_framework.urlpatterns import format_suffix_patterns
+
+# urlpatterns = [
+#     path('', views.snippet_list),
+#     path('snippets/<int:pk>/', views.snippet_detail),
+# ]
+
+# urlpatterns = format_suffix_patterns(urlpatterns)
+
+# ####################################################################
+# #BT - Class base view - ApiView
+# ####################################################################
+
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
-from rest_framework.urlpatterns import format_suffix_patterns
-
 urlpatterns = [
-    path('', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    path('', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
